c3dd2fd9f7299f29dd22b9fa9ec99140a6cbd0d9
Ryan Leach <rleach@rleach.id.au>
2014-11-12 07:05:53 +1030

    Now compiles in gradlew.bat

--- src/main/java/au/id/rleach/memorystones/MemoryStonesPlugin.java
+++ src/main/java/au/id/rleach/memorystones/MemoryStonesPlugin.java
@@ -1,6 +1,7 @@
 package au.id.rleach.memorystones;
+import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = "MemoryStones", name = "MemoryStones" version="0.0.1-SNAPSHOT")
+@Plugin(id = "MemoryStones", name = "MemoryStones", version="0.0.1-SNAPSHOT")
 public class MemoryStonesPlugin {
 
 }
